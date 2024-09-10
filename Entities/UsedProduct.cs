3480972a124adf422ec7dcb784cb0f54bbf2e206
Gabriel Guislotti <gabrielguislotti@hotmail.com>
2024-09-10 07:31:04 -0300

    Exercicio da Udemy

---
+++ Entities/UsedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace UdemyExercicios2.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+    }
+}
